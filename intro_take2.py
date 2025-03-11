b808de3747a4761dc42e0ca32e59c31f33901676
imakena2 <imakena@gmail.com>
2025-03-11 21:25:58 +0300

    Introduction To Python

---
+++ intro_take2.py
@@ -0,0 +1,17 @@
+# Integer (Whole number)
+age = 30
+
+# Float (Decimal number)
+height = 7.8945
+
+# String (Text) - Add quotes
+name = "Idah"
+
+# Boolean (True/False)
+student = True
+
+# Check Data Type
+print(type(age))      # Output: <class 'int'>
+print(type(height))   # Output: <class 'float'>
+print(type(name))     # Output: <class 'str'>
+print(type(student))  # Output: <class 'bool'>
---
+++ lesson2.py
@@ -0,0 +1,8 @@
+number=10
+print("Hello world")
+# Datatypes
+myname= "Idah"
+print(myname)
+
+languages = [1,2,4 5,8]
+print(languages[2])
