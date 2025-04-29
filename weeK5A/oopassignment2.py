d91283280a11856b54c784caa0cc1feb5b33e9fc
imakena2 <imakena@gmail.com>
2025-04-29 14:23:38 +0300

    Add week 5 OOP polymorphism assignment

---
+++ weeK5A/oopassignment2.py
@@ -0,0 +1,13 @@
+class Car:
+    def move(self):
+        print("Car is driving")
+
+class Plane:
+    def move(self):
+        print("Plane is flying")
+
+my_car = Car()
+my_plane = Plane()
+
+my_car.move()
+my_plane.move()
